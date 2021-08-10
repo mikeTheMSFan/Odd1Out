5e3755966210dd57928a428b9bcdc3d77173bfcd
mikeTheMSFan <64504996+mikeTheMSFan@users.noreply.github.com>
2021-08-10 17:54:15 -0400

    Updated code to add separations of concern and changed logo style

--- js/site.js
+++ js/site.js
@@ -1,4 +1,54 @@
-// Call Hello World
-function helloWorld() {
-  alert('Hello World');
+'use strict';
+
+//get the values fro the Page
+//start or controller function
+function getValues() {
+  //get values from the page
+  let startValue = document.getElementById('startValue').value;
+  let endValue = document.getElementById('endValue').value;
+
+  //parse into integers
+  startValue = parseInt(startValue);
+  endValue = parseInt(endValue);
+
+  //We need to validate our input
+  if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+    //we call generateNumbers
+    let numbers = generateNumbers(startValue, endValue);
+    //we call displayNumbers
+    displayNumbers(numbers);
+  } else {
+    alert('You must enter integers');
+  }
+}
+
+//generate numbers from startValuee to the endValue
+//logic function(s)
+function generateNumbers(startValue, endValue) {
+  let numbers = [];
+
+  //we want to get all numbers from start to end
+  for (let i = startValue; i <= endValue; i++) {
+    //this will execute in a loop until i = endValue.
+    numbers.push(i);
+  }
+  return numbers;
+}
+
+//display the number and mark even numbers bold
+//display or view functions
+function displayNumbers(numbers) {
+  let templateRows = '';
+  for (let i = 0; i < numbers.length; i++) {
+    let number = numbers[i];
+    let className = 'even';
+    if (number % 2 === 0) {
+      className = 'even';
+    } else {
+      className = 'odd';
+    }
+    templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+  }
+
+  document.getElementById('results').innerHTML = templateRows;
 }
